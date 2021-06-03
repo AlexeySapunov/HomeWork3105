044dc4d3a2788acdc57684a8bc1f88480bf3f1f5
Alexey <blacka8607@rambler.ru>
2021-06-03 12:45:53 +0300

    Update HomeWork3105

--- src/main/java/ru/geekbrainsHW7/Main.java
+++ src/main/java/ru/geekbrainsHW7/Main.java
@@ -1,11 +1,24 @@
 package ru.geekbrainsHW7;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        Cat cat = new Cat("Barsik", 5);
+        Cat cat = new Cat("Barsik", 5, false);
         Plate plate = new Plate(100);
         plate.info();
-        cat.eat(plate);
+        cat.Eat(plate);
         plate.info();
+        Plate plate1 = new Plate(10);
+        Cat[] cats = new Cat[3];
+        cats[0] = new Cat("Boris", 10, false);
+        cats[1] = new Cat("Vaska", 20, false);
+        cats[2] = new Cat("Mursik", 15, false);
+        IntStream.range(0, cats.length).forEach(i -> {
+            cats[i].Eat(plate1);
+            System.out.println("cat " + cats[i].getName() + "'s satiety " + cats[i].isSatiety());
+            if (!cats[i].isSatiety()) {
+                plate1.addFood();
+            }
+        });
     }
 }
